000c8f874a0d52deec96887cbd36f1eb791f368e
kzitouni <zkarim091019@gmail.com>
2021-03-18 12:15:39 -0400

    checkout item upgrade

--- components/Checkout/CartItem.js
+++ components/Checkout/CartItem.js
@@ -1,42 +1,61 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
+BiTrash;
+import { BiTrash } from "react-icons/bi";
 export default function CartItem({ data, updateNums, index, handleRemove }) {
   const [Quantity, updateQuantity] = useState(data.quantity);
   const [removed, updateRemoved] = useState(false);
 
   function handleChange(e) {
     e.preventDefault();
-    updateQuantity(e.target.value);
-    updateNums(e.target.value, index);
+    if (Number(e.target.value) > -1 && Number(e.target.value) < 10) {
+      updateQuantity(Number(e.target.value));
+      updateNums(e.target.value, index);
+    }
   }
 
+  function handleAdd() {
+    let newQuantity = Number(Quantity) + 1;
+    if (newQuantity > -1 && newQuantity < 10) {
+      updateQuantity(newQuantity);
+      updateNums(newQuantity, index);
+    }
+  }
+
+  function handleSubtract() {
+    let newQuantity = Quantity - 1;
+    if (newQuantity > -1 && newQuantity < 10) {
+      updateQuantity(newQuantity);
+      updateNums(newQuantity, index);
+    }
+  }
   return (
     <>
       {!removed && (
         <Wrapper>
           <Container>
-            <Image src={data.img} />
-            <Product>
-              <H1>{data.name}</H1>
-              <Color>
-                {data.size} - {data.color}
-              </Color>
+            <LeftCont>
+              <Image src={data.img} />
+              <TextCont>
+                <H1>{data.name}</H1>
+                <Subtitle>
+                  {data.size} - {data.color}
+                </Subtitle>
+              </TextCont>
+            </LeftCont>
+            <RightCont>
+              <QuantityCont>
+                <MinusIcon onClick={() => handleSubtract()} />
+
+                <NumCont>
+                  <Num value={Quantity} onChange={(e) => handleChange(e)} />
+                </NumCont>
+                <PlusIcon onClick={() => handleAdd()} />
+              </QuantityCont>
               <Price>${(data.price * data.quantity).toFixed(2)}</Price>
-              <Select value={Quantity} onChange={(e) => handleChange(e)}>
-                <Option value={1}>1</Option>
-                <Option value={2}>2</Option>
-                <Option value={3}>3</Option>
-                <Option value={4}>4</Option>
-                <Option value={5}>5</Option>
-                <Option value={6}>6</Option>
-                <Option value={7}>7</Option>
-                <Option value={8}>8</Option>
-                <Option value={9}>9</Option>
-                <Option value={10}>10</Option>
-              </Select>
-              <Remove onClick={() => handleRemove(index)}>Remove</Remove>
-            </Product>
+              <TrashIcon onClick={() => handleRemove(index)} />
+            </RightCont>
           </Container>
         </Wrapper>
       )}
@@ -56,70 +75,137 @@ const Container = styled.div`
   width: 100%;
   height: auto;
   display: flex;
-  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
   flex-direction: row;
   border: 1px solid #dadcdf;
-  padding: 20px;
-  @media (max-width: 1200px) {
-    width: 75%;
-  }
+  height: 90px;
+
   @media (max-width: 1000px) {
     width: 100%;
   }
 `;
 const Image = styled.img`
-  width: 250px;
-  height: 250px;
-  margin-right: 50px;
-  @media (max-width: 1200px) {
-    width: 250px;
-    height: 250px;
-  }
-  @media (max-width: 700px) {
-    width: 125px;
-    height: 125px;
+  width: 70px;
+  height: 70px;
+  border-radius: 10px;
+  @media (max-width: 400px) {
+    width: 50px;
+    height: 50px;
   }
 `;
-const Product = styled.div`
-  width: 50%;
+
+const TextCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* max-width: 200px; */
+  max-width: 70%;
+  margin-left: 10px;
 `;
+
+const Subtitle = styled.h1`
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 12px;
+  color: #858585;
+`;
+
 const H1 = styled.h1`
   font-family: Helvetica, Arial, sans-serif;
   font-size: 14px;
   color: #2f3337;
   padding-bottom: 15px;
+  font-weight: 700;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 `;
-const Color = styled.div`
-  font-family: Helvetica, Arial, sans-serif;
-  font-size: 14px;
-  color: #2f3337;
-  padding-bottom: 15px;
-`;
-const Price = styled.div`
+
+const Price = styled.h1`
   font-weight: 700;
   font-family: Helvetica, Arial, sans-serif;
-  font-size: 20px;
-  padding-bottom: 15px;
-`;
-const Select = styled.select`
-  background: transparent !important;
-  border: 1px solid #e7e8ea;
-  padding-bottom: 1px;
-  padding-left: 10px;
-  height: 34px;
-  width: 40%;
-  border-radius: 4px;
-  margin-bottom: 75px;
-  @media (max-width: 500px) {
-    -moz-appearance: none;
-    -webkit-appearance: none;
-    padding: none;
-    width: 60%;
+  font-size: 18px;
+  margin-left: 20px;
+  @media (max-width: 1000px) {
+    margin-left: 35px;
+  }
+  @media (max-width: 400px) {
+    margin-left: 0px;
+    margin-bottom: 10px;
   }
 `;
-const Option = styled.option``;
-const Remove = styled.div`
-  color: #2f3337;
-  text-decoration: underline;
+const LeftCont = styled.div`
+  margin-right: 10px;
+  margin-left: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  max-width: 50%;
+  @media (max-width: 1000px) {
+    max-width: 40%;
+  }
+`;
+const RightCont = styled.div`
+  margin-right: 10px;
+  margin-left: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  max-width: 40%;
+  @media (max-width: 1000px) {
+    max-width: 50%;
+  }
+  @media (max-width: 400px) {
+    flex-direction: column-reverse;
+  }
+`;
+
+const PlusIcon = styled(AiOutlinePlus)`
+  margin-left: 10px;
   cursor: pointer;
+`;
+
+const MinusIcon = styled(AiOutlineMinus)`
+  margin-right: 10px;
+  cursor: pointer;
+`;
+
+const TrashIcon = styled(BiTrash)`
+  margin-left: 20px;
+  color: #dfdfdf;
+  font-size: 22px;
+  min-width: 22px;
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+  :hover {
+    color: #ff5656;
+  }
+  @media (max-width: 400px) {
+    display: none;
+  }
+`;
+
+const QuantityCont = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const NumCont = styled.div`
+  border: 1px solid #edeeef;
+  height: 25px;
+  width: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 3px;
+`;
+
+const Num = styled.input`
+  font-family: Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 600;
+  width: 100%;
+  color: unset;
+  border: unset;
+  text-align: center;
 `;
